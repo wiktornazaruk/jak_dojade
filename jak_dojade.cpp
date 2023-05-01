94804f9f9c9c24be80d25a97d1b6241b118533c1
Wiktor Nazaruk <wiktor.nazaruk7@gmail.com>
2023-05-01 22:49:08 +0200

    works everything except printing path

--- jak_dojade.cpp
+++ jak_dojade.cpp
@@ -1,35 +1,192 @@
 #include <iostream>
 #include <string>
-#include <list>
-#include <vector>
 using namespace std;
 #define EMPTY '.'
 #define ROAD '#'
 #define CITY '*'
 
-struct Pair
+struct AdjListNode
 {
-	int id, edge;
+	int dest;
+	int weight;
+	struct AdjListNode* next;
 };
 
-class Graph {
-public:
-	vector<list<Pair>> adj;
-	Graph(int size) :adj(size) { }
-	void addEdge(int id, Pair pair) {
-		adj[id].push_back(pair);
-	}
-	void print()
+struct AdjList
+{
+	struct AdjListNode* head;
+};
+
+struct Graph
+{
+	int V;
+	struct AdjList* array;
+};
+
+struct AdjListNode* newAdjListNode(int dest, int weight)
+{
+	struct AdjListNode* newNode = (struct AdjListNode*) malloc(sizeof(struct AdjListNode));
+	newNode->dest = dest;
+	newNode->weight = weight;
+	newNode->next = NULL;
+	return newNode;
+}
+
+struct Graph* createGraph(int V)
+{
+	struct Graph* graph = (struct Graph*) malloc(sizeof(struct Graph));
+	graph->V = V;
+
+	graph->array = (struct AdjList*) malloc(V * sizeof(struct AdjList));
+
+	for (int i = 0; i < V; ++i)
+		graph->array[i].head = NULL;
+
+	return graph;
+}
+
+void addEdge(struct Graph* graph, int src, int dest, int weight)
+{
+	struct AdjListNode* newNode = newAdjListNode(dest, weight);
+	newNode->next = graph->array[src].head;
+	graph->array[src].head = newNode;
+
+	newNode = newAdjListNode(src, weight);
+	newNode->next = graph->array[dest].head;
+	graph->array[dest].head = newNode;
+}
+
+struct MinHeapNode
+{
+	int  v;
+	int dist;
+};
+
+struct MinHeap
+{
+
+	int size;
+	int capacity;
+	int* pos;
+	struct MinHeapNode** array;
+};
+
+struct MinHeapNode* newMinHeapNode(int v, int dist)
+{
+	struct MinHeapNode* minHeapNode = (struct MinHeapNode*) malloc(sizeof(struct MinHeapNode));
+	minHeapNode->v = v;
+	minHeapNode->dist = dist;
+	return minHeapNode;
+}
+
+struct MinHeap* createMinHeap(int capacity)
+{
+	struct MinHeap* minHeap = (struct MinHeap*) malloc(sizeof(struct MinHeap));
+	minHeap->pos = (int*)malloc(capacity * sizeof(int));
+	minHeap->size = 0;
+	minHeap->capacity = capacity;
+	minHeap->array = (struct MinHeapNode**) malloc(capacity *sizeof(struct MinHeapNode*));
+	return minHeap;
+}
+
+void swapMinHeapNode(struct MinHeapNode** a,struct MinHeapNode** b)
+{
+	struct MinHeapNode* t = *a;
+	*a = *b;
+	*b = t;
+}
+
+void minHeapify(struct MinHeap* minHeap,int idx)
+{
+	int smallest, left, right;
+	smallest = idx;
+	left = 2 * idx + 1;
+	right = 2 * idx + 2;
+
+	if (left < minHeap->size &&
+		minHeap->array[left]->dist <
+		minHeap->array[smallest]->dist)
+		smallest = left;
+
+	if (right < minHeap->size &&
+		minHeap->array[right]->dist <
+		minHeap->array[smallest]->dist)
+		smallest = right;
+
+	if (smallest != idx)
 	{
-		for (int i = 0; i < adj.size(); i++)
-		{
-			for (auto v : adj[i])
-				cout << i << " " << v.id << " " << v.edge << "\n";
-		}
+		MinHeapNode* smallestNode = minHeap->array[smallest];
+		MinHeapNode* idxNode = minHeap->array[idx];
+
+		minHeap->pos[smallestNode->v] = idx;
+		minHeap->pos[idxNode->v] = smallest;
+
+		swapMinHeapNode(&minHeap->array[smallest], &minHeap->array[idx]);
+
+		minHeapify(minHeap, smallest);
 	}
+}
 
+int isEmpty(struct MinHeap* minHeap)
+{
+	return minHeap->size == 0;
+}
 
-};
+struct MinHeapNode* extractMin(struct MinHeap* minHeap)
+{
+	if (isEmpty(minHeap))
+		return NULL;
+
+	struct MinHeapNode* root = minHeap->array[0];
+
+	struct MinHeapNode* lastNode = minHeap->array[minHeap->size - 1];
+	minHeap->array[0] = lastNode;
+
+	minHeap->pos[root->v] = minHeap->size - 1;
+	minHeap->pos[lastNode->v] = 0;
+
+	--minHeap->size;
+	minHeapify(minHeap, 0);
+
+	return root;
+}
+
+void decreaseKey(struct MinHeap* minHeap, int v, int dist)
+{
+	int i = minHeap->pos[v];
+
+	minHeap->array[i]->dist = dist;
+
+	while (i && minHeap->array[i]->dist < minHeap->array[(i - 1) / 2]->dist)
+	{
+		minHeap->pos[minHeap->array[i]->v] = (i - 1) / 2;
+		minHeap->pos[minHeap->array[(i - 1) / 2]->v] = i;
+		swapMinHeapNode(&minHeap->array[i], &minHeap->array[(i - 1) / 2]);
+
+		i = (i - 1) / 2;
+	}
+}
+
+bool isInMinHeap(struct MinHeap* minHeap, int v)
+{
+	if (minHeap->pos[v] < minHeap->size)
+		return true;
+	return false;
+}
+
+void printArr(int dist[], int n, int src)
+{
+	cout << endl;
+	cout << "Source id: " << src << endl;
+	for (int i = 0; i < n; ++i)
+		cout << "Destination id: " << i << " distance: " << dist[i] << endl;
+}
+
+void printShortestTravelTimeBetweenCities(int dist[], int dest)
+{
+	cout << dist[dest];
+}
+
 
 struct City
 {
@@ -50,6 +207,74 @@ struct Query
 	// If the type is zero, the query is only for the time. If the type is one, the route should also be provided.
 	bool type;
 };
+
+void printPath(int currentVertex, int* parents, int numOfCities, City* cities)
+{
+	if (currentVertex == -1) {
+		return;
+	}
+	printPath(parents[currentVertex], parents, numOfCities, cities);
+	cout << " " << cities[currentVertex].name;
+}
+
+void dijkstra(struct Graph* graph, int src, int dest, int type, int numOfCities, City* cities)
+{
+	int V = graph->V;
+	int* dist = new int[V];
+	int* parents = new int[V];
+
+	parents[src] = -1;
+
+	struct MinHeap* minHeap = createMinHeap(V);
+
+	for (int v = 0; v < V; ++v)
+	{
+		dist[v] = INT_MAX;
+		minHeap->array[v] = newMinHeapNode(v, dist[v]);
+		minHeap->pos[v] = v;
+		parents[v] = -1;
+	}
+
+	minHeap->array[src] = newMinHeapNode(src, dist[src]);
+	minHeap->pos[src] = src;
+	dist[src] = 0;
+	decreaseKey(minHeap, src, dist[src]);
+
+	minHeap->size = V;
+
+	while (!isEmpty(minHeap))
+	{
+		struct MinHeapNode* minHeapNode = extractMin(minHeap);
+
+		int u = minHeapNode->v;
+
+		if (u == dest) break;
+
+		struct AdjListNode* pCrawl = graph->array[u].head;
+		while (pCrawl != NULL)
+		{
+			int v = pCrawl->dest;
+
+			if (isInMinHeap(minHeap, v) && dist[u] != INT_MAX && pCrawl->weight + dist[u] < dist[v])
+			{
+				parents[v] = u;
+				decreaseKey(minHeap, v, dist[v]);
+				dist[v] = dist[u] + pCrawl->weight;
+			}
+			pCrawl = pCrawl->next;
+		}
+	}
+
+	//printArr(dist, V, src);
+	printShortestTravelTimeBetweenCities(dist, dest);
+	if (type == 1)
+	{
+		//print intermediate cities
+		printPath(dest, parents, numOfCities, cities);
+	}
+
+	cout << endl;
+}
 
 bool isInMap(int x, int y, int height, int width)
 {
@@ -174,19 +399,16 @@ void checkPos(Point& pos, char** map, int numOfCities, City* cities, Graph& g, C
 	}
 	else if (map[pos.y][pos.x] == CITY)
 	{
-		Pair p;
-		p.edge = pos.dist;
 		int i = 0;
 		while (i < numOfCities)
 		{
 			if (cities[i].x == pos.x && cities[i].y == pos.y)
 			{
-				p.id = i;
 				break;
 			}
 			i++;
 		}
-		g.addEdge(city.id, p);
+		addEdge(&g, city.id, i, pos.dist);
 	}
 }
 
@@ -219,7 +441,7 @@ void bfs(Graph& g, City city, char **map, int h, int w, int numOfCities, City* c
 			s.x++;
 			checkPos(s, map, numOfCities, cities, g, city);
 			visited[s.y][s.x] = true;
-			if (map[s.y][s.x] == CITY) return;
+			//if (map[s.y][s.x] == CITY) return;
 			q.push(s);
 			s.x--;
 		}
@@ -227,7 +449,7 @@ void bfs(Graph& g, City city, char **map, int h, int w, int numOfCities, City* c
 			s.x--;
 			checkPos(s, map, numOfCities, cities, g, city);
 			visited[s.y][s.x] = true;
-			if (map[s.y][s.x] == CITY) return;
+			//if (map[s.y][s.x] == CITY) return;
 			q.push(s);
 			s.x++;
 		}
@@ -235,7 +457,7 @@ void bfs(Graph& g, City city, char **map, int h, int w, int numOfCities, City* c
 			s.y++;
 			checkPos(s, map, numOfCities, cities, g, city);
 			visited[s.y][s.x] = true;
-			if (map[s.y][s.x] == CITY) return;
+			//if (map[s.y][s.x] == CITY) return;
 			q.push(s);
 			s.y--;
 		}
@@ -243,7 +465,7 @@ void bfs(Graph& g, City city, char **map, int h, int w, int numOfCities, City* c
 			s.y--;
 			checkPos(s, map, numOfCities, cities, g, city);
 			visited[s.y][s.x] = true;
-			if (map[s.y][s.x] == CITY) return;
+			//if (map[s.y][s.x] == CITY) return;
 			q.push(s);
 			s.y++;
 		}
@@ -251,7 +473,7 @@ void bfs(Graph& g, City city, char **map, int h, int w, int numOfCities, City* c
 
 }
 
-void makeGraph(Graph& g, City*& cities, int numOfCities, char** map, int h, int w )
+void fillGraph(Graph& g, City*& cities, int numOfCities, char** map, int h, int w )
 {
 	int y, x, i = 0;
 	for (y = 0; y < h; y++)
@@ -312,17 +534,9 @@ void addFlightsToGraph(Flight* flights, int k, City* cities, int numOfCities, Gr
 		}
 		if (id1 != -1 && id2 != -1)
 		{
-			Pair p;
-			p.id = id2;
-			p.edge = flights[i].time;
-			g.addEdge(id1, p);
+			addEdge(&g, id1, id2, flights[i].time);
 		}
 	}
-}
-
-void dijkstra(Graph g, int src, int dest, int type)
-{
-
 }
 
 int main()
@@ -377,10 +591,10 @@ int main()
 		}
 	}
 
-	Graph g(numOfCities);
-	makeGraph(g, cities, numOfCities, map, h, w);
-	addFlightsToGraph(flights, k, cities, numOfCities, g);
-	g.print();
+	Graph* g = createGraph(numOfCities);
+	fillGraph(*g, cities, numOfCities, map, h, w);
+	addFlightsToGraph(flights, k, cities, numOfCities, *g);
+	//g.print();
 
 	// print map
 	/*cout << endl;
@@ -416,7 +630,7 @@ int main()
 			}
 		}
 		if(src != -1 && dest != -1)
-			dijkstra(g, src, dest, queries[i].type);
+			dijkstra(g, src, dest, queries[i].type, numOfCities, cities); 
 	}
 	
 }
